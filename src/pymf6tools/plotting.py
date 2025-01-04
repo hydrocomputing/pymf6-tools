6846a4a1e4f792bc0bf7a225039d47521ac37ec9
Mike Müller <mmueller@python-academy.de>
2025-01-04 16:22:07 +0100

    Formatting, remove unused imports

--- src/pymf6tools/plotting.py
+++ src/pymf6tools/plotting.py
@@ -1,11 +1,7 @@
-"""Plot model results.
-"""
-import os
-import sys
+"""Plot model results."""
 
 from matplotlib import pyplot as plt
 from matplotlib.patches import Patch
-import matplotlib as mpl
 import numpy as np
 import flopy
 from flopy.utils.postprocessing import get_specific_discharge
@@ -14,15 +10,15 @@ from pymf6tools.make_model import get_simulation
 
 
 def show_heads(
-        model_path,
-        name,
-        title='',
-        show_grid=True,
-        show_wells=True,
-        show_contours=True,
-        kstpkper=(119, 2),
-        spdis_index=240
-        ):
+    model_path,
+    name,
+    title='',
+    show_grid=True,
+    show_wells=True,
+    show_contours=True,
+    kstpkper=(119, 2),
+    spdis_index=240,
+):
     """Plot calculated heads along with flow vector."""
     sim = get_simulation(model_path, name)
     gwf_name = 'gwf_' + name
@@ -35,7 +31,7 @@ def show_heads(
     spdis = bud.get_data(text='DATA-SPDIS')[spdis_index]
     qx, qy, _ = get_specific_discharge(spdis, gwf)
     pmv = flopy.plot.PlotMapView(gwf)
-    levels=np.arange(0.2, 1.4, 0.02)
+    levels = np.arange(0.2, 1.4, 0.02)
     arr = pmv.plot_array(head)
     if show_grid:
         pmv.plot_grid(colors='white')
@@ -43,27 +39,23 @@ def show_heads(
         pmv.contour_array(head, levels=levels)
     if show_wells:
         pmv.plot_bc(name='wel', plotAll=True, kper=kstpkper[-1])
-    plot = pmv.plot_vector(
-        qx,
-        qy,
-        normalize=True,
-        color="white")
+    plot = pmv.plot_vector(qx, qy, normalize=True, color='white')
     plot.axes.set_xlabel('x (m)')
     plot.axes.set_ylabel('y (m)')
     plot.axes.set_title(title)
-    #ticks = np.arange(0, 1.41, 0.1)
-    cbar = plot.get_figure().colorbar(arr) # ticks=ticks)
+    # ticks = np.arange(0, 1.41, 0.1)
+    cbar = plot.get_figure().colorbar(arr)  # ticks=ticks)
     cbar.set_label('Groundwater level (m)')
     return plot
 
 
-
 def show_bcs(
-        model_path,
-        name,
-        title='Boundary Conditions',
-        bc_names = ('chd', 'wel', 'riv'),
-        show_grid=True):
+    model_path,
+    name,
+    title='Boundary Conditions',
+    bc_names=('chd', 'wel', 'riv'),
+    show_grid=True,
+):
     """Show location of boundary conditions."""
     handles = []
     gwf_name = 'gwf_' + name
@@ -78,6 +70,7 @@ def show_bcs(
         color = bc.cmap.colors[-1]
         handles.append(Patch(facecolor=color, edgecolor='k', label=name))
         return bc
+
     for bc_name in bc_names:
         plot = add_bc(bc_name)
     if show_grid:
@@ -88,19 +81,22 @@ def show_bcs(
     plot.axes.legend(handles=handles, loc=(1.2, 0))
     return plot
 
+
 def show_concentration(
-        model_path, name,
-        title='',
-        layer=0,
-        show_grid=True,
-        levels=None,
-        kstpkper=None,
-        show_wells=True,
-        show_rivers=True,
-        vmin=None,
-        vmax=None,
-        show_contours=True,
-        show_arrows=False,):
+    model_path,
+    name,
+    title='',
+    layer=0,
+    show_grid=True,
+    levels=None,
+    kstpkper=None,
+    show_wells=True,
+    show_rivers=True,
+    vmin=None,
+    vmax=None,
+    show_contours=True,
+    show_arrows=False,
+):
     """Plot calculated heads along with flow vector."""
     gwtname = 'gwt_' + name
     sim = get_simulation(model_path, name)
@@ -117,7 +113,9 @@ def show_concentration(
     if show_wells:
         plot = pmv.plot_bc(package=gwf.get_package('wel'), plotAll=True, kper=1)
     if show_rivers:
-        plot = pmv.plot_bc(package=gwf.get_package('riv-1'), plotAll=True, kper=1)
+        plot = pmv.plot_bc(
+            package=gwf.get_package('riv-1'), plotAll=True, kper=1
+        )
     if show_contours:
         pmv.contour_array(
             conc,
@@ -133,25 +131,22 @@ def show_concentration(
         bud = gwf.output.budget()
         spdis = bud.get_data(text='DATA-SPDIS')[240]
         qx, qy, _ = get_specific_discharge(spdis, gwf)
-        plot = pmv.plot_vector(
-            qx,
-            qy,
-            normalize=True,
-            color="white")
+        plot = pmv.plot_vector(qx, qy, normalize=True, color='white')
     return arr
 
 
 def show_well_head(
-        wel_coords,
-        model_path,
-        model_name,
-        times,
-        title='',
-        y_start=0.3,
-        y_end=1.05,
-        upper_head_limit=None,
-        lower_head_limit=None,
-        x=(0, 32)):
+    wel_coords,
+    model_path,
+    model_name,
+    times,
+    title='',
+    y_start=0.3,
+    y_end=1.05,
+    upper_head_limit=None,
+    lower_head_limit=None,
+    x=(0, 32),
+):
     """Plot head at well over time."""
     sim = get_simulation(model_path, model_name)
     gwf_name = 'gwf_' + model_name
@@ -159,7 +154,7 @@ def show_well_head(
     # ml = sim.get_model(model_name)
     print(gwf.output)
     heads = gwf.output.head().get_ts(wel_coords)
-    time = gwf.output.budget().get_data(text="SPDIS")[times]
+    time = gwf.output.budget().get_data(text='SPDIS')[times]
     _, ax = plt.subplots()
     ax.plot(heads[:, 0], heads[:, 1], label='Well water level')
     ax.set_xlabel('Time (d)')
@@ -182,7 +177,7 @@ def show_well_head(
         limit_range = True
         text += ' range'
         y1 = [lower_head_limit] * 2
-        y2 =[upper_head_limit] * 2
+        y2 = [upper_head_limit] * 2
     elif lower_head_limit is not None:
         one_limit = True
         y1 = [lower_head_limit] * 2
@@ -190,26 +185,28 @@ def show_well_head(
         one_limit = True
         y1 = [upper_head_limit] * 2
     if one_limit or limit_range:
-        ax.plot(x, y1, color='red', linestyle=':',
-                label=text)
+        ax.plot(x, y1, color='red', linestyle=':', label=text)
     if limit_range:
         ax.plot(x, y2, color='red', linestyle=':')
     ax.plot(
-         x_stress_1, y_stress,
-         color='lightblue', linestyle=':', label='Stress periods')
-    ax.plot(
-         *x_stresses,
-         color='lightblue', linestyle=':')
+        x_stress_1,
+        y_stress,
+        color='lightblue',
+        linestyle=':',
+        label='Stress periods',
+    )
+    ax.plot(*x_stresses, color='lightblue', linestyle=':')
     ax.legend(loc=(1.1, 0))
     return ax
 
+
 def contour_well_heads(
-        model_path,
-        name,
-        title='',
-        show_wells=True,
-        show_inactive=False,
-        ):
+    model_path,
+    name,
+    title='',
+    show_wells=True,
+    show_inactive=False,
+):
     """Plot calculated heads with contour in the vector field."""
     sim = get_simulation(model_path, name)
     gwf = sim.get_model(name)
@@ -225,36 +222,26 @@ def contour_well_heads(
     pmv.plot_grid()
 
     if show_wells:
-        pmv.plot_bc(name="WEL", plotAll=True, kper=1)
-    plot = pmv.plot_vector(
-        qx,
-        qy,
-        normalize=True,
-        color="grey")
+        pmv.plot_bc(name='WEL', plotAll=True, kper=1)
+    plot = pmv.plot_vector(qx, qy, normalize=True, color='grey')
     plot.axes.set_xlabel('x (m)')
     plot.axes.set_ylabel('y (m)')
     plot.axes.set_title(title)
-    cbar = plot.get_figure().colorbar(contour_set) # ticks=ticks)
+    cbar = plot.get_figure().colorbar(contour_set)  # ticks=ticks)
     cbar.set_label('Groundwater level (m)')
     return plot
 
 
-def show_bot_elevations(
-        model_path,
-        model_name,
-        max_top,
-        max_botm,
-        layer
-        ):
-    """Plot model bottom elevations"""
+def show_bot_elevations(model_path, model_name, max_top, max_botm, layer):
+    """Plot model bottom elevations."""
     sim = get_simulation(model_path, model_name)
     ml = sim.get_model(model_name)
-    #get packages of the model
+    # get packages of the model
     dis = ml.get_package('dis')
     riv = ml.get_package('riv')
-    #extract first stress period array
+    # extract first stress period array
     riv1 = riv.stress_period_data.array[0]
-    #extracting botm array from discretization package
+    # extracting botm array from discretization package
     array = dis.botm.array[0]
     # get the data from the bottom elevations for respective coordinates of river
     for entry in riv1:
@@ -269,37 +256,32 @@ def show_bot_elevations(
     pmv.ax.set_title(str(layer) + ' layer - model bottoms')
 
     ticks = np.arange(max_botm, max_top, 0.5)
-    #plot color bar
+    # plot color bar
     cbar = botm_arr.get_figure().colorbar(botm_arr, ticks=ticks)
     cbar.set_label('m')
 
     return pmv
 
-def contour_bot_elevations(
-        model_path,
-        model_name,
-        max_top,
-        max_botm,
-        layer
-        ):
-    """Plot model bottom elevations"""
+
+def contour_bot_elevations(model_path, model_name, max_top, max_botm, layer):
+    """Plot model bottom elevations."""
     sim = get_simulation(model_path, model_name)
     ml = sim.get_model(model_name)
-    #get packages of the model
+    # get packages of the model
     dis = ml.get_package('dis')
     riv = ml.get_package('riv')
-    #extract first stress period array
+    # extract first stress period array
     riv1 = riv.stress_period_data.array[0]
-    #extracting botm array from discretization package
+    # extracting botm array from discretization package
     array = dis.botm.array[0]
     # get the data from the bottom elevations for respective coordinates of river
     for entry in riv1:
         coordinate = entry['cellid'][layer:]
         array[coordinate] = entry['rbot']
 
-    fig = plt.figure(figsize=(5,5))
-    ax = fig.add_subplot(1, 1, 1, aspect="equal")
-    ax.set_title("Model Contour Bottom Elevation ")
+    fig = plt.figure(figsize=(5, 5))
+    ax = fig.add_subplot(1, 1, 1, aspect='equal')
+    ax.set_title('Model Contour Bottom Elevation ')
     pmv = flopy.plot.PlotMapView(model=ml, layer=layer)
     botm_arr = pmv.contour_array(array)
     pmv.plot_grid()
@@ -308,23 +290,24 @@ def contour_bot_elevations(
     pmv.ax.set_title(str(layer) + ' layer - model bottoms')
 
     ticks = np.arange(max_botm, max_top, 0.5)
-    #plot color bar
+    # plot color bar
     cbar = fig.colorbar(botm_arr)
     cbar.set_label('m')
 
     return pmv
 
+
 def plot_spec_discharge(
-        model_path,
-        model_name,
-        layer,
-        times,
-        show_inactive=False,
-        ):
+    model_path,
+    model_name,
+    layer,
+    times,
+    show_inactive=False,
+):
     """Plot model specific discharge to the respective layer."""
     sim = get_simulation(model_path, model_name)
     ml = sim.get_model(model_name)
-    spdis = ml.output.budget().get_data(text="SPDIS")[times]
+    spdis = ml.output.budget().get_data(text='SPDIS')[times]
     head = ml.output.head().get_alldata()[0]
     qx, qy, _ = get_specific_discharge(spdis, ml)
 
@@ -335,26 +318,22 @@ def plot_spec_discharge(
     if show_inactive:
         pmv.plot_inactive()
 
-    plt.title("Specific discharge layer " + str(layer))
+    plt.title('Specific discharge layer ' + str(layer))
     plt.colorbar(quadmesh)
 
     return pmv
 
 
-def show_river_stages(
-        model_path,
-        model_name,
-        layer,
-        time_period):
-    """Plot model bottom elevations"""
+def show_river_stages(model_path, model_name, layer, time_period):
+    """Plot model bottom elevations."""
     sim = get_simulation(model_path, model_name)
     ml = sim.get_model(model_name)
-    #get packages of the model
+    # get packages of the model
     dis = ml.get_package('dis')
     riv = ml.get_package('riv')
-    #extract first stress period array
+    # extract first stress period array
     riv1 = riv.stress_period_data.array[time_period]
-    #extracting botm array from discretization package
+    # extracting botm array from discretization package
     array = dis.top.array
     # get the data from the bottom elevations for respective coordinates of river
     for entry in riv1:
@@ -368,8 +347,8 @@ def show_river_stages(
     pmv.ax.set_ylabel('y (m)')
     pmv.ax.set_title(str(layer) + ' layer - river stage')
 
-    #ticks = np.arange(min(array), max(riv1), 0.5)
-    #plot color bar
+    # ticks = np.arange(min(array), max(riv1), 0.5)
+    # plot color bar
     cbar = botm_arr.get_figure().colorbar(botm_arr)
     cbar.set_label('m')
 
@@ -415,16 +394,6 @@ def show_river_stages(
 #     cbar.set_label('m')
 
 #     return pmv
-
-
-
-
-
-
-
-
-
-
 
 
 # fig = plt.figure(figsize=(8, 8))
