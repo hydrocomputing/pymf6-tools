0909f4d6c2c070f939b0389a81ae80439a15b66a
Mike Müller <mmueller@python-academy.de>
2025-03-16 15:54:15 +0100

    Store results in HDF5 files

--- tests/mf6_examples/run.py
+++ tests/mf6_examples/run.py
@@ -1,7 +1,12 @@
 """Setup a  model."""
 
-from pymf6_tools.mf6examples.run_models import main
+from pymf6_tools.mf6examples.run_models import run_and_store_models
+from pymf6_tools.mf6examples.process_results import store_dfs
 
+
+def main(pickle_file='results.pcl',):
+    run_and_store_models(pickle_file)
+    store_dfs(pickle_file='results.pcl')
 
 if __name__ == '__main__':
     main()
