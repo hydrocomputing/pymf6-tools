80e0694322e6ccb461079945798c475f78e7da0e
Mike Müller <mmueller@python-academy.de>
2025-03-26 16:43:19 +0100

    Modfiy controlled model ex-gwt-mt3dsupp632a/mf6gwf

--- tests/mf6_examples/controlled/ex-gwt-mt3dsupp632a/mf6gwf/run.py
+++ tests/mf6_examples/controlled/ex-gwt-mt3dsupp632a/mf6gwf/run.py
@@ -7,9 +7,17 @@ from pymf6.mf6 import MF6
 
 def run(sim_path):
     """Run."""
-    for _ in MF6(sim_path=sim_path).model_loop():
+    sim = MF6(sim_path=sim_path, do_solution_loop=False)
+    gwf_models = sim.models['gwf6']
+    gwf = gwf_models['flow']
+    loop = sim.model_loop()
+    for _ in loop:
+        if gwf.kper > 0:
+            break
+    wel = gwf.packages.get_package('wel-1').as_mutable_bc()
+    wel.q = 0.06
+    for _ in loop:
         pass
-
 
 if __name__ == '__main__':
     run(sys.argv[1])
